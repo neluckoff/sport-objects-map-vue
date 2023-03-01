b622af666fb2e5e3751d4b46686f7ae3de16b002
luckoff <neluckoff@gmail.com>
2023-03-01 15:26:42 +0300

    fixed graph

--- backend/app/internal/routes/objects.py
+++ backend/app/internal/routes/objects.py
@@ -6,6 +6,14 @@ from app.package.database.tools import Database
 router = APIRouter(prefix='/api/v1')
 db = Database()
 
-@router.get('/objects')
+@router.get('/objects/open')
 def get_objects():
-    return db.get_objects()
+    return db.get_online_objects()
+
+@router.get('/objects/closed')
+def get_objects():
+    return db.get_offline_objects()
+
+@router.get('/financing/{id}')
+def get_objects(id: int):
+    return db.get_cash_object(id_object=id)
--- backend/app/package/database/tools.py
+++ backend/app/package/database/tools.py
@@ -17,29 +17,50 @@ class Database:
         self.__cursor = self.__db.cursor()
         # self.__create_tables()
         
-    def get_objects(self):
+    def get_online_objects(self):
         objects = []
 
         self.__cursor.execute("SELECT * FROM `sports_objects` WHERE `is_active` = 'Y' LIMIT 800")
         for row in self.__cursor.fetchall():
-            if row[12]:
-                sport = row[12].title()
-            else:
-                sport = row[12]
-            if row[3]:
-                desc = row[3].replace('&#40', '').replace('&#41', '')
-            else:
-                desc = row[3]
             objects.append({
                 "id": row[0],
                 "name": row[1],
-                "desc": desc,
+                "desc": row[3].replace('&#40', '').replace('&#41', '').replace('&#37', '') if row[3] else row[3],
                 "active": row[2],
                 "address": row[7],
                 "action": row[10].title(),
-                "objectType": row[11].title(),
-                "sportType": sport,
+                "objectType": row[11].title() if row[11] else row[11],
+                "sportType": row[12].title() if row[12] else row[12],
                 "coordinates": {"lat": row[13], "lng": row[14]}
             }
-            )
+        )
         return objects
+    
+    
+    def get_offline_objects(self):
+        objects = []
+
+        self.__cursor.execute("SELECT * FROM `sports_objects` WHERE `is_active` = 'N' LIMIT 800")
+        for row in self.__cursor.fetchall():
+            objects.append({
+                "id": row[0],
+                "name": row[1],
+                "desc": row[3].replace('&#40', '').replace('&#41', '').replace('&#37', '') if row[3] else row[3],
+                "active": row[2],
+                "address": row[7],
+                "action": row[10].title(),
+                "objectType": row[11].title() if row[11] else row[11],
+                "sportType": row[12].title() if row[12] else row[12],
+                "coordinates": {"lat": row[13], "lng": row[14]}
+            }
+        )
+        return objects
+    
+    
+    def get_cash_object(self, id_object: int):
+        cash = []
+        
+        self.__cursor.execute(f"SELECT `financing_federal`, `financing_subject`, `financing_municipal`, `financing_outside` FROM `sports_objects` WHERE `id` = '{id_object}'")
+        for row in self.__cursor.fetchone():
+            cash.append(row)
+        return cash
